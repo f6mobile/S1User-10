551e9047b02cb3fbb52bfdadb547da9f16faaf14
user <user@C0905.local>
2022-06-16 15:45:43 +0300

    Initial commit

---
+++ app/src/main/java/com/denis/demo/MainActivity4.java
@@ -0,0 +1,14 @@
+package com.denis.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
